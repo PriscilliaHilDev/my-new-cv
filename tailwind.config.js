32ebbd54917ea65a0df74d9426d681d5d2f81fc4
PriscilliaHilDev <priscillia.hillion@outlook.fr>
2025-09-10 00:07:22 -0400

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media', // ou 'media' si tu veux basé sur le système
   content: [
     "./*.html",       // Tous les fichiers HTML à la racine
     "./src/js/**/*.js" // Tous les fichiers JavaScript dans le dossier src/js
